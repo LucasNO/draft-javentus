efa8de12c00bdafc36cc4f863642b6866183b1a3
Lucas Norberto <lucas.oliveira@vitaebrasil.com.br>
2017-11-01 09:59:06 -0200

    make it better

---
+++ src/test/java/com/draft/javentus/JaventusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.draft.javentus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JaventusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
